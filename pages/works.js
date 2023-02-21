021f3fff0a936f7c18a5301903a39534759c50fd
G-SF <gabrielsmangor@gmail.com>
2023-02-21 15:07:55 -0300

    projects sidebar test

--- pages/works.js
+++ pages/works.js
@@ -1,4 +1,4 @@
-import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import { Container, SimpleGrid, Box, useColorModeValue } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
@@ -11,9 +11,16 @@ import thumbHandGesture from '../public/images/works/handgesture-home.png'
 const Works = () => (
   <Layout title="Works">
     <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        css={{ backdropFilter: 'blur(10px)' }}
+      >
         Projects
-      </Heading>
+      </Box>
 
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section>
